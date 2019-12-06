80133af65ee5937b22578f54a601491dfeff1ed8
Adam J. Forster <adamf@bullionbypost.co.uk>
2019-12-06 13:43:18 +0000

    Solve 2019 day 1 part 2

---
+++ 2019/day1/part2.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+
+def calculate_fuel_for_mass(mass):
+    return mass // 3 - 2
+
+
+def calculate_fuel_for_fuel(fuel_mass):
+    print(fuel_mass)
+    new_fuel = calculate_fuel_for_mass(fuel_mass)
+    if new_fuel > 0:
+        fuel_mass += calculate_fuel_for_fuel(new_fuel)
+    return fuel_mass
+
+
+with open('input.txt', 'r') as f:
+    print(sum(calculate_fuel_for_fuel(calculate_fuel_for_mass(int(mass))) for mass in f))
